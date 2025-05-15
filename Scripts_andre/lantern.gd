889f1b0b3d763e3f2771e83c804384bc24f850ba
André Tavares <andre.t.teixeira@hotmail.com>
2025-05-14 22:03:56 -0300

    Código detecção lanterna
    Mudanças e evolução no código da lanterna

--- Scripts_andre/lantern.gd
+++ Scripts_andre/lantern.gd
@@ -3,20 +3,24 @@ extends PointLight2D
 var monsterInLight
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float):
+	var space_state = get_world_2d().direct_space_state
+	#if monsterInLight:
+		#var result = space_state.intersect_ray(position, monsterInLight.position, [self])
+		#if result && result.collider.name == "Monster":
+			#print("Hit monster")
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D):
 	if body.name == "Monster":
-		monsterInLight = body # Replace with function body.
+		monsterInLight = body #
 
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
+func _on_area_2d_body_exited(body: Node2D):
 	if body.name == "Monster":
-		monsterInLight = null # Replace with function body.
+		monsterInLight = null #
--- Scripts_andre/player.gd
+++ Scripts_andre/player.gd
@@ -3,13 +3,23 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
-
+var motion = Vector2()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+		$AnimatedSprite2D.play("default")
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+		$AnimatedSprite2D.play("default")
+		$AnimatedSprite2D.flip_h = true
+	else:
+		motion.x = 0
+		$AnimatedSprite2D.play("default")
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY

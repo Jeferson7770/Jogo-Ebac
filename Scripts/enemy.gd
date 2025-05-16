9abee3143b428eafbd5c614a528bd13663738d74
jeferson7770 <jefersonmoreira770@gmail.com>
2025-05-16 13:54:14 -0300

    atualização da lanterna paralizante

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,48 +1,54 @@
 extends CharacterBody2D
 
 # Variáveis do inimigo
-@export var speed: float = 50
+@export var speed: float = 130
 @export var health: int = 100
-@export var stop_distance: float = 5
-@onready var wall_detector :=$Wall_detector as RayCast2D
+@export var stop_distance: float = 1
 @onready var player = null
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@export var jump_force: float = -300.0
+@onready var wall_detector := $Wall_detector as RayCast2D
+
+# Nova variável pra controle de paralisia
+var is_paralyzed: bool = false
 
 func _ready():
 	player = get_parent().get_node("player")
 
 func _physics_process(delta):
-	
-	
-	# Aplica gravidade se não estiver no chão
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	
+	# Se estiver paralisado, não faz nada
+	if is_paralyzed:
+		velocity = Vector2.ZERO
+	else:
+		# Se tiver player na cena
+		if player:
+			var distance = global_position.distance_to(player.global_position)
 
-	# Persegue o player
-	if player:
-		var distance = global_position.distance_to(player.global_position)
-		
-		if distance > stop_distance:
-			var direction = (player.global_position - global_position).normalized()
-			velocity.x = direction.x * speed
-			flip(direction)
-		else:
-			velocity.x = 0
-	
+			# Se estiver longe, voa na direção do player
+			if distance > stop_distance:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = direction * speed  # Movimento completo em X e Y
+				flip(direction)
+			else:
+				velocity = Vector2.ZERO
+
+	# Move o inimigo
 	move_and_slide()
 
 func flip(direction: Vector2):
+	# Mantém virado para a direção horizontal de movimento
 	if direction.x > 0:
 		$AnimatedSprite2D.scale.x = 1  # olha pra direita
 	elif direction.x < 0:
-		$AnimatedSprite2D.scale.x = -1  # olha pra esquerda
+		$AnimatedSprite2D.scale.x = -1   # olha pra esquerda
 
 func take_damage(amount: int):
 	health -= amount
 	if health <= 0:
 		queue_free()
+
+# Novo método para definir paralisia
+func set_paralyzed(state: bool):
+	is_paralyzed = state
+
 	
 
 	

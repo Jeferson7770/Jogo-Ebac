e1a6907e69724164171141433a446e944b87ee01
André Tavares <andre.t.teixeira@hotmail.com>
2025-05-17 16:49:47 -0300

    Correções Inimigos

--- Scripts/Lanterna/lookAtMouse.gd
+++ Scripts/Lanterna/lookAtMouse.gd
@@ -26,6 +26,8 @@ func _process(delta):
 func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("Enemy"):
 		body.set_paralyzed(true)
+	#if body.is_in_group("shine"):
+		
 
 
 func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:

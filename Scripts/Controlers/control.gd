c669c179c9bb1d61e85272d1ba3201b3f7dc113d
André Tavares <andre.t.teixeira@hotmail.com>
2025-05-17 19:15:42 -0300

    Tela de creditos

--- Scripts/Controlers/control.gd
+++ Scripts/Controlers/control.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var sprite: Sprite2D = $VBoxContainer/Creditos/creditos
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/fase_prototipo.tscn") # Replace with function body.
@@ -8,3 +8,12 @@ func _on_start_pressed() -> void:
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
+
+
+
+func _on_creditos_pressed() -> void:
+	sprite.visible = true
+
+
+func _on_close_pressed() -> void:
+	sprite.visible = false

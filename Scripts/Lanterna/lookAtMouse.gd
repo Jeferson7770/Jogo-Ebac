18a00e50e8d44dcb0658982c9477818beddee0de
André Tavares <andre.t.teixeira@hotmail.com>
2025-05-17 22:12:46 -0300

    Edições

--- Scripts/Lanterna/lookAtMouse.gd
+++ Scripts/Lanterna/lookAtMouse.gd
@@ -1,6 +1,8 @@
 extends Sprite2D
 
 @export var rotation_speed: float = 5.0 # Ajuste este valor para mudar a velocidade da rotação
+@export var no_da_porta: NodePath
+var porta_alvo: Node2D 
 
 # Chamado a cada frame. 'delta' é o tempo decorrido desde o frame anterior.
 func _process(delta):
@@ -27,9 +29,35 @@ func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 	if body.is_in_group("Enemy"):
 		body.set_paralyzed(true)
 	#if body.is_in_group("shine"):
-		
+		#timer >= 2s
+		#desativa porta
 
 
 func _on_area_2d_body_shape_exited(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("Enemy"):
 		body.set_paralyzed(false)
+		
+		
+
+func _ready():
+	# Verifica se o caminho para o nó da porta foi definido e obtém o nó.
+	if not no_da_porta.is_empty():
+		porta_alvo = get_node_or_null(no_da_porta)
+		
+
+func _on_area_entered(area: Area2D):
+	# Verifica se a área que entrou pertence ao grupo "chaves"
+	if area.is_in_group("chaves"):
+		print("Chave detectada pelo cone de visão!")
+		desativar_porta()
+		
+
+func desativar_porta():
+	if porta_alvo:
+		print("Desativando a porta.")
+		# Opção 1: Tornar a porta invisível
+		porta_alvo.visible = false
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
